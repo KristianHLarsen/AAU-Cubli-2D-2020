fc749eee58d691a5cfba43fa5da590f9a666a2ff
IvelinPenchev <ivelin.penchev.95@gmail.com>
2020-10-26 14:13:05 +0100

    Updated touchdown

---
+++ cubli2D_code/Touchdown_code/cubli2D_code_touchdown/cubli2D_code_touchdown_v2/cubli2D_code_touchdown_v2.ino
@@ -0,0 +1,148 @@
+//Code for the Cubli2D
+//This code is a mix of Cubli2D code from group 730(2019) and 733(2020)
+
+
+//libraries
+#include <stdlib.h>
+#include <Servo.h>
+#include <VidorFPGA.h>
+#include <Wire.h>
+
+//pin definitions
+#define enable 1      //Enable motor: HIGH = on, LOW = off
+#define imuIn 3       //IMU_switch PIN
+#define potIn 4       //POT_switch PIN
+#define potPin A1     //POT_analogValue
+#define PWM_PIN 45    //Output pin for pwm on digital 5 on vidor
+#define BRAKE_PIN 2   //Servo_digital pin
+#define SPEED_PIN A5  //Speed from driver
+
+//mechanical and system constants
+#define CURRENT_MAX 7.5           //max current
+#define recovery 40*3.14/180.0    //The Cubli has fallen
+#define rpm2rad 2.0*3.14/60.0     //Conversion from RPM to Radian/s
+#define kt 0.0335                 //Motor Constant
+
+//self-adjusting Balance
+#define CHECK_CYCLE 50
+#define reject 10
+#define step_size 0.1*0.01
+#define large_step_size 0.1*0.1
+float  ANGLE_REF = 0.5, cycle_speed[81];
+int cycle = 0, reject_count = 0, running_average = 0;
+bool add_cycle = false;
+
+//servo
+Servo brake;
+#define brk 100  //Servo position when braking
+#define go 110   //Servo position when running
+
+//controller
+#define k1 -0.0141//-0.0316       //SpeedOfWheel
+#define k2 -9.9079//-15.6593      //AngleError
+#define k3 -0.5//-0.5             //SpeedOfFrame
+#define k1_pot -0.001             //SpeedOfWheel - Potentiometer     
+#define k2_pot -1.0               //AngleError - Potentiometer
+#define k3_pot -0.1               //SpeedOfFrame - Potentiometer
+
+//sensor
+#define scale 31.8    //Scale for gyroscope (From datasheet)
+#define MPU_addr 0x68 //I2C address for the IMU. (CODE WILL BE STUCK IF NOT CORRECT!)
+double acc_angle_1[2] = {0, 0}, gyro_angle_1[2] = {0, 0}, comp_angle_1[2] = {0, 0}, AcX, AcY, GyZ, az, z;
+int ogsens, sensor = 0 ;  // 2 for imu and 1 for potentiometer, 0 for off
+float mean = 1.0, spw = 0.0, spf = 0.0, angle = 0.0, angle_last = 0.0, angle_speed = 0.0,angle_speed2 = 0.0, ang_err = 0.0, curr = 0.0;
+
+//complementary filter
+double kc1, kc2, tau = 0.3471;
+
+//PWM
+#define bits 7
+int freq_max = 90, freq_min = 10, freq_mid = 50, duty = 0;
+
+//misc functionality
+int samp_period = 15000;
+int sam_start = 0;
+int touchdown_measurement_timer = 0;
+int sam_slut = 0;
+int timer_var = 0, time_now = 0, time_last = 0;
+
+// touchdown vars
+#define TD_ANGLE_START 2*3.14/180.0
+#define TD_GAIN 3*10.6286
+
+int last_sensor= 0;
+int touchdown_timer= 0;
+boolean touchdown_start= false;
+float touchdown_curr = 0;
+int touchdown_pwm = 50;
+bool enable_touchdown = true;
+boolean touchdown_brake = false;
+float spf_pot = 0;
+
+
+
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial1.begin(115200);
+
+  IMUConfig();
+
+  brake.attach(BRAKE_PIN);
+  brake.write(go);
+  for (int a = 0; a < CHECK_CYCLE; a++)  cycle_speed[a] = 0.0;
+
+  PWMConfig();
+
+  pinMode(enable, OUTPUT);
+  pinMode(imuIn, INPUT_PULLUP);
+  pinMode(potIn, INPUT_PULLUP);
+  
+  filter_setup();
+  
+  brake.write(brk);  //brake motor
+  delay(1000);
+  brake.write(go);   // release brake
+  delay(1000);
+  time_last = millis();
+  sam_start = micros();
+  sam_slut = micros();
+}
+
+
+
+void loop() {
+  if (digitalRead(imuIn) == LOW) { // if IMU is choosen physically
+    sensor = 2;
+    ogsens = 2;
+    last_sensor= 2;
+    samp_period = 2000; // sampling period
+    touchdown_start = false;
+    touchdown_brake = false;
+  }
+  else if (digitalRead(potIn) == LOW) { // if POT is choosen physically
+    sensor = 1;
+    ogsens = 1;
+    last_sensor= 1;
+    samp_period = 15000; // sampling period
+    touchdown_start = false;
+    touchdown_brake = false;
+  }
+  else sensor = 0; // if system is off
+  if (sensor) {
+    digitalWrite(enable, HIGH); //enable driver for writing
+    if (micros() - timer_var >= samp_period) updateMotor(); // if we have waited the sampling time.
+    balancePoint();  // check if ANGLE_REF needs correction
+    standup();   //Did we fall? then rise.
+  }
+  if (!sensor) { // sytem is off
+    if(enable_touchdown) touchDown();
+    else digitalWrite(enable, LOW); // disable driver
+
+    print_touchdown_data();
+    timer_var = 0; // reset time
+    time_now = 0;  // reset
+//    time_last = 0; // reset
+  }
+}
---
+++ cubli2D_code/Touchdown_code/cubli2D_code_touchdown/cubli2D_code_touchdown_v3/motor_standup_balance.ino
@@ -0,0 +1,169 @@
+float interpolate(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+float speed_frame() {
+  if (sensor == 1 || sensor == 0) {
+    time_now = micros();
+    angle_speed = ((ang_err - angle_last) / ((float)(time_now - time_last) / 1000000.0)); //0.0018
+    time_last = time_now;
+    angle_last = ang_err;
+  }
+  if (sensor == 2) angle_speed = (GyZ * 0.0174532925);  //Gyroscope is speed of frame
+  return angle_speed;
+}
+
+float speed_frame_imu() {
+  return (GyZ * 0.0174532925);  //Gyroscope is speed of frame
+}
+
+void standup() {
+  sensor = 1;
+  if (abs(angle_pot()) > recovery) {  // if we are down
+    FPGA.analogWrite(PWM_PIN, map(50, 0, 100, pow(2, bits), 0)); //stop motor
+    delay(200);        //Let it fall completely
+    if (abs((((float)(((float)analogRead(SPEED_PIN) - 512)) * (2048.0 / 1024.0)) * rpm2rad)) > 100.0) {
+      for (int o = 0; o < 11; o++) { // if we are above a certain speed use ABS brake for not tipping over
+        brake.write(brk + 3);
+        delay(30);
+        brake.write(go);
+        delay(50);
+      }
+    }
+    brake.write(brk);
+    delay(300);
+    brake.write(go);
+    if (angle_pot() > 0) { //determine the way we fell and spin up accordingly
+      FPGA.analogWrite(PWM_PIN, map(45, 0, 100, pow(2, bits), 0)); //Determine the direction we fell
+      delay(3000);
+    }
+    else {
+      FPGA.analogWrite(PWM_PIN, map(55, 0, 100, pow(2, bits), 0));
+      delay(3500);
+    }
+    brake.write(brk - 5); //brake motor hard to get up properly
+    delay(75);            //wait before disabling the brake
+    brake.write(go);      // release brake
+    int g = micros();
+    add_cycle = true;     //Disable ANGLE_REF corrections
+    while (micros() - g < 1000000) {   //wait for system to be stable before giving the IMU control
+      updateMotor();
+      delay(10);
+    }
+    filter_setup();
+    add_cycle = false;     //ENABLE ANGLE_REF corrections
+    sam_start = micros();  //Reset timing parameter since last reading
+    timer_var = micros();  // --//--
+    if (abs(ANGLE_REF) > 15.0) { // if the angle ref somehow changed a lot, move it back.
+      if (ANGLE_REF > 0.0) ANGLE_REF = 14.0;
+      if (ANGLE_REF < 0.0) ANGLE_REF = -14.0;
+    }
+  }
+  sensor = ogsens;         //Reset to original sensor before standup
+}
+
+void balancePoint() { // Change ANGLE_REF if we are at constant wheel velocity
+  if (cycle == CHECK_CYCLE) { // if we have enough readings
+    for (int x = 0; x < CHECK_CYCLE; x++)   running_average = running_average + cycle_speed[x];
+    running_average = running_average / CHECK_CYCLE;
+
+    for (int b = 0; b < CHECK_CYCLE; b++)
+    { // check if the readings is far away from the average, then we dont change anything
+      if (abs(cycle_speed[b]) < abs(running_average) * 0.8 || abs(cycle_speed[b]) > abs(running_average) * 1.2)   reject_count++;
+      if (reject_count == reject)  break;
+    }
+    if (reject_count != reject && abs(running_average) <= 100 * rpm2rad ) { // small step, close to equilibrium
+      if (running_average < -step_size)  ANGLE_REF = ANGLE_REF - step_size;
+      if (running_average > step_size)   ANGLE_REF = ANGLE_REF + step_size;
+    }
+    if (reject_count != reject && abs(running_average) > 100 * rpm2rad) { // large step, far away from equilibrium
+      if (running_average < -step_size)  ANGLE_REF = ANGLE_REF - large_step_size; //left of eq
+      if (running_average > step_size)   ANGLE_REF = ANGLE_REF + large_step_size; //right of eq
+    }
+    running_average = 0; //reset values
+    reject_count = 0;
+    cycle = 0;
+  }
+
+}
+
+void updateMotor() {
+  sam_slut = micros(); // meassure time
+  timer_var = micros();
+  ang_err =  angle_pot();      // calculate angle error
+//  Serial.println("Balance:");
+//  Serial.print(ang_err);
+  spf = speed_frame();         // calculate frame speed
+  spw = (((float)(((float)analogRead(SPEED_PIN) - 512)) * (2048.0 / 1024.0)) * rpm2rad); // measure flywheel speed
+  if (sensor == 1)  curr = (((k1_pot * spw + k2_pot * ang_err + k3_pot * spf)) / kt); // potentiometer controller
+  if (sensor == 2)  curr = (((k1 * spw + k2 * ang_err + k3 * spf)) / kt);  // IMU controller
+  if (curr >= CURRENT_MAX)  curr = CURRENT_MAX;  // if above max current set equal to max
+  if (curr <= -CURRENT_MAX)  curr = -CURRENT_MAX; // if below min current set equal to min
+  duty = (int)interpolate(curr, -CURRENT_MAX, CURRENT_MAX, freq_max, freq_min); // map the current from max to min
+  FPGA.analogWrite(PWM_PIN, map(duty, 0, 100, pow(2, bits), 0)); // set pwm of the motor
+  if (!add_cycle) cycle_speed[cycle] = spw; // save the speed for the ANGLE_REF correction
+  if (!add_cycle) cycle++; // add one to the array
+  sam_start = timer_var;
+}
+
+void touchDown () {
+
+  if(touchdown_start == false)
+  {
+    FPGA.analogWrite(PWM_PIN, map(50, 0, 100, pow(2, bits), 0)); //stop motor
+    touchdown_timer= millis();
+    touchdown_start = true;
+  }
+  ang_err =  angle_pot();
+//  Serial.print(ang_err);
+
+  if (abs(ang_err) > recovery - 21*3.14/180 && !touchdown_brake)
+  {
+      brake.write(brk - 5); //brake motor hard to get up properly 
+//      delay(75);            //wait before disabling the brake
+      touchdown_measurement_timer = millis();
+      Serial.println("in loop");
+      while (millis() - touchdown_measurement_timer <= 75) // printing data to serial port while braking
+      {        
+        print_touchdown_data ();   
+      }
+      Serial.println("out of loop");
+      brake.write(go);      // release brake 
+      touchdown_brake = true;
+  }
+
+
+  if (abs(ang_err) > recovery || millis() - touchdown_timer> 1200)
+    {
+//      Serial.println("Stopping now"); 
+      FPGA.analogWrite(PWM_PIN, map(50, 0, 100, pow(2, bits), 0)); //stop motor
+      digitalWrite(enable, LOW); // disable driver
+    }
+  else if (abs(ang_err) > 0.035)
+  {
+    touchdown_curr = touchdown_gain * sin(ang_err);
+    if (touchdown_curr > CURRENT_MAX) touchdown_curr = CURRENT_MAX;
+    if (touchdown_curr < -CURRENT_MAX) touchdown_curr = -CURRENT_MAX;
+    touchdown_pwm = (int)interpolate(touchdown_curr, -CURRENT_MAX, CURRENT_MAX, freq_max, freq_min); // map the current from max to min
+    FPGA.analogWrite(PWM_PIN, map(touchdown_pwm, 0, 100, pow(2, bits), 0)); // set pwm of the motor
+
+  }  
+}
+
+void print_touchdown_data()
+{
+    filter_setup();
+    
+    spf_pot = speed_frame();
+    ang_err =  angle_pot();
+    spf_imu = speed_frame_imu();
+
+    Serial.print(millis());
+    Serial.print("; ");
+    Serial.print(ang_err);
+    Serial.print("; ");
+    Serial.print(spf_pot);
+    Serial.print("; ");
+    Serial.println(spf_imu);
+  
+}
