bc45d0658b5c4492106ece604b1e826911a4d0df
KristianHLarsen <37039278+KristianHLarsen@users.noreply.github.com>
2020-11-12 11:52:42 +0100

    Update delay_estimator_dev1.ino

--- communication/Delay_estimator_v1/delay_estimator_dev1/delay_estimator_dev1.ino
+++ communication/Delay_estimator_v1/delay_estimator_dev1/delay_estimator_dev1.ino
@@ -92,9 +92,18 @@ void DelayTransmit(uint8_t cmd) {
 // 'B'  "stand up with the brake"
 // 'C'  "Im standing up"
 // 'D'  "Shutdown"
+unsigned long delay_est_timer = 0;
+unsigned long delay_est_timer_last = 0;
+unsigned long delay_time[20] =0;
+unsigned long delay_timeout = 40000; // Delay timeout set to 20 ms.
+int delay_array_handler = 0;
 
-void receive() {
+unsigned long receive() {
   if (Serial1.available() > PACKET_SIZE ) {
+    unsigned long delay_time_est =0;
+    delay_est_timer = micros();
+    if(delay_array_handler > 19)delay_array_handler = 0;
+    delay_time[delay_array_handler] = delay_est_timer-delay_est_timer_last;
     uint8_t cmdtemp = Serial1.read();
     if (cmdtemp == 'L'||cmdtemp =='R' ||cmdtemp == 'S'||cmdtemp =='V' ||cmdtemp == 'B'||cmdtemp =='C' ) {
       // rxcmd= Serial.read();
@@ -121,7 +130,16 @@ void receive() {
       rxdelaybuffer.push(DelayDataFull);
       cmdtemp = 0;
     }
-
+    delay_est_timer_last = delay_est_timer;
+    for (int i=0; i<19; i++){
+      delay_time_est = delay_time[i]+delay_time_est;
+    }
+    delay_time_est = delay_time_est/20
+    Serial.println(delay_time_est);
+    return delay_time_est;
+  }
+  if( delay_timeout < micros() - delay_est_timer_last){
+    return 0;
   }
 }
 // get retrieved from buffer value using following command:
@@ -147,7 +165,7 @@ void countdown(){
 }
 
 //void packet_timeout(){
-  
+
 //}
 
 int i = 4;
@@ -161,8 +179,8 @@ int i = 4;
 // Parameters used for time estimation:
 unsigned long tstop = 0, tdelay = 0, timer_var = 0;
 unsigned long tstart[3000];
-const unsigned long packet_timeout=100000; 
-const unsigned long ts = 8000; // Sample time for the system: 
+const unsigned long packet_timeout=100000;
+const unsigned long ts = 8000; // Sample time for the system:
 int PacketNumber = 4;
 int packetdelay=0;
 
@@ -178,29 +196,32 @@ void setup() {
   delay(5000);
   Serial.print("Packet Size:  ");Serial.println((sizeof(delay_est)+1));
   countdown();
-  timer_var = 0; 
+  timer_var = 0;
   flag = 0;
 }
 
-
+unsigned long delay =0;
 void loop() {
-receive();
+delay = receive();
+if (delay == 0)Serial.println("Timeout");
+if (delay > 0){Serial.print("Delay:  "); Serial.println(delay);}
+
 
   if (micros() - timer_var >= ts) {
     timer_var = micros();
     tstart[PacketNumber] = micros();
     i++;
-    txdelay.packet.PacketNumber = PacketNumber; 
+    txdelay.packet.PacketNumber = PacketNumber;
   //  Serial.print("Packet number: "); Serial.print(PacketNumber); Serial.print(" |  ");
     PacketNumber = PacketNumber+1;
     DelayTransmit('D');
-  
+
  /*
   // transmit packet back dev1
   if (rxdelaybuffer.size() >= 8) {
     if (micros() - timer_var >= ts) {
     timer_var = micros();
-    if (rxdelaybuffer.isEmpty() != true) { // check if buffer is empty: if not then transmit packet. 
+    if (rxdelaybuffer.isEmpty() != true) { // check if buffer is empty: if not then transmit packet.
       get_delay_data();
       txdelay.packet.PacketNumber = DelayDataFull.PacketNumber;
       DelayTransmit('D');
@@ -209,30 +230,30 @@ receive();
   }
 */
  //if (rxdelaybuffer.size() >= 20){
- // flag =1;    
+ // flag =1;
     if (rxdelaybuffer.isEmpty() != true) { // Print 1 element from buffer.
       get_delay_data();
      packetdelay = Dtempdata.PacketNumber;
      //packetdelay= packetdelay;
       tstop = (micros() - tstart[packetdelay]);
-      //Serial.print("Delay 2 way: "); 
-      Serial.println(tstop); 
+      //Serial.print("Delay 2 way: ");
+      Serial.println(tstop);
       //Serial.print("[us]"); Serial.print("PacketNumber: "); Serial.println(packetdelay);
     }
   }
  //}
- 
+
    // Print test result dev2
   if (rxdelaybuffer.size() >= 1 ) {
      get_delay_data();
      packetdelay = Dtempdata.PacketNumber;
      //packetdelay= packetdelay;
       tstop = (micros() - tstart[packetdelay]);
-      //Serial.print("Delay 2 way: "); 
-      Serial.println(tstop); 
+      //Serial.print("Delay 2 way: ");
+      Serial.println(tstop);
       //Serial.print("[us]"); Serial.print("PacketNumber: "); Serial.println(packetdelay);
     }
-if(i>2998){ // Reset counter to avoid overflow. 
+if(i>2998){ // Reset counter to avoid overflow.
 i = 3 ;
 PacketNumber = 0;
 }
